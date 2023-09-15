7c0735ad38026ba7a26e5838ec3704f0a684ed3b
hiroka <mementoka@gmail.com>
2023-09-15 17:11:10 +0900

    fix link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,23 @@
-import OldLinks from "./OldLinks";
+import Link from 'next/link';
 
+const OldLinks = () => {
+  const oldLink = 'http://cse.eedept.kobe-u.ac.jp/old_webdata/event/workshop';
+  const recentLink = 'http://cse.eedept.kobe-u.ac.jp/uww';
+  const uwwYears = new Array(16).fill(0).map((v, i) => v+i+2007);
+
+  return (
+    <div className='text-center rounded m-2'>
+      <h1 className='text-2xl font-bold p-1'>Links</h1>
+      <div className='grid grid-cols-4 p-1 text-lg'>
+        {uwwYears.map((year, i) => (
+          year < 2009
+            ? <Link key={i} href={oldLink + year} className='hover:underline'>UWW{year}</Link>
+            : <Link key={i} href={recentLink + year} className='hover:underline'>UWW{year}</Link>
+        ))}
+      </div>
+    </div>
+  );
+}
 const Footer = () => {
   return (
     <footer className='text-center py-2 w-full bg-pink-100'>
