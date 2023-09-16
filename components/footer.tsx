3dedface6a96f2fbf71008a018acb255d3d8022f
hiroka <mementoka@gmail.com>
2023-09-16 15:55:59 +0900

    fix email and description

--- components/footer.tsx
+++ components/footer.tsx
@@ -21,7 +21,7 @@ const OldLinks = () => {
 const Footer = () => {
   return (
     <footer className='text-center py-2 w-full bg-pink-100'>
-      <h2 className='text-center text-lg'>E-mail: uww2022org [at] ubi.eedept.kobe-u.ac.jp</h2>
+      <h2 className='text-center text-lg'>E-mail: uww2023org [at] ubi.eedept.kobe-u.ac.jp</h2>
       <OldLinks />
       <p>© Tsukamoto Terada Lab.</p>
     </footer>
