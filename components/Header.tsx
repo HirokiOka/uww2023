5ee91ea64c1ddb3f8ab763896c4b70c56e40f500
hiroka <mementoka@gmail.com>
2023-10-19 23:23:30 +0900

    fix Home URL

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <>
     <header className='bg-blue-200 top-0 fixed w-full'>
       <nav className='my-3 mx-6 text-xl text-right font-bold'>
-        <Link href="/" className='m-2 px-2 hover:underline'>Home</Link>
+        <Link href="http://s2-lab.sakura.ne.jp/uww2023" className='m-2 px-2 hover:underline'>Home</Link>
         <Link href="http://www.yumebutai.org/access/index.html" className='m-2 px-2 hover:underline'>Access</Link>
       </nav>
     </header>
