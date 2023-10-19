c6f1355fde07b14bc15a0f977187471228d1defe
hiroka <mementoka@gmail.com>
2023-10-19 23:20:31 +0900

    fix URL

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,6 @@ const Header = () => {
     <header className='bg-blue-200 top-0 fixed w-full'>
       <nav className='my-3 mx-6 text-xl text-right font-bold'>
         <Link href="/" className='m-2 px-2 hover:underline'>Home</Link>
-        <Link href="/" className='m-2 px-2 hover:underline'>Program</Link>
         <Link href="http://www.yumebutai.org/access/index.html" className='m-2 px-2 hover:underline'>Access</Link>
       </nav>
     </header>
--- components/Registration.tsx
+++ components/Registration.tsx
@@ -3,7 +3,7 @@ const Registration = () => {
     <div className='pt-8 m-3 px-4 md:px-24 font-serif'>
       <h2 className='text-2xl md:text-4xl font-mono font-bold'>Registration Fees</h2>
       <div className='rounded mx-2 p-3'>
-        <p className='text-2xl px-2'>参加費: TBD</p>
+        <p className='text-2xl px-2'>参加費</p>
         <p className='text-lg px-4'>一般: 19,000円 (会議参加費10,000円、宿泊費9,000円)</p>
         <p className='text-lg px-4'>学生: 16,000円 (会議参加費7,000円、宿泊費9,000円)</p>
       </div>
