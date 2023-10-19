b4ff781e36e8f2c3bb97ef6b481c83d3e8b601ce
hiroka <mementoka@gmail.com>
2023-10-19 23:06:22 +0900

    add registration info

--- components/Registration.tsx
+++ components/Registration.tsx
@@ -4,6 +4,8 @@ const Registration = () => {
       <h2 className='text-2xl md:text-4xl font-mono font-bold'>Registration Fees</h2>
       <div className='rounded mx-2 p-3'>
         <p className='text-2xl px-2'>参加費: TBD</p>
+        <p className='text-lg px-4'>一般: 19,000円 (会議参加費10,000円、宿泊費9,000円)</p>
+        <p className='text-lg px-4'>学生: 16,000円 (会議参加費7,000円、宿泊費9,000円)</p>
       </div>
     </div>
   );
