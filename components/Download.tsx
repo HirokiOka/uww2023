dd1fec776c1c906e29673586da6cf1d7ab9a7e7c
hiroka <mementoka@gmail.com>
2023-11-03 11:42:58 +0900

    fix http to https

--- components/Download.tsx
+++ components/Download.tsx
@@ -5,8 +5,8 @@ const Download = () => {
       <div className='rounded mx-4 p-2'>
         <div className='text-center inline-flex'> 
          <h3 className='text-xl mx-auto font-serif'>原稿テンプレート：</h3>
-            <button className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mx-6"><a href="http://s2-lab.sakura.ne.jp/uww2023/data/UWW2023_Format_LaTeX.zip">LaTeX</a></button>
-             <button className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mx-6"><a href="http://s2-lab.sakura.ne.jp/uww2023/data/word_UWW2023.docx">Word</a></button>
+            <button className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mx-6"><a href="https://s2-lab.sakura.ne.jp/uww2023/data/UWW2023_Format_LaTeX.zip">LaTeX</a></button>
+             <button className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mx-6"><a href="https://s2-lab.sakura.ne.jp/uww2023/data/word_UWW2023.docx">Word</a></button>
         </div>
       </div>
     </div>
