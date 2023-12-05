0cb7effd80136e11df4fb7acb7f4a94431b3d92d
hiroka <mementoka@gmail.com>
2023-12-05 10:55:07 +0900

    add link to program page

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ const Header = () => {
     <header className='bg-blue-200 top-0 fixed w-full'>
       <nav className='my-3 mx-6 text-xl text-right font-bold'>
         <Link href="https://tt-lab.jp/uww2023" className='m-2 px-2 hover:underline'>Home</Link>
+        <Link href="https://tt-lab.jp/uww2023/program" className='m-2 px-2 hover:underline'>Program</Link>
         <Link href="https://www.yumebutai.org/access/index.html" className='m-2 px-2 hover:underline'>Access</Link>
       </nav>
     </header>
